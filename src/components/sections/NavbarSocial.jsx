e64bda7421352b160eb07ca61af4986e521e7465
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-21 11:37:41 -0300

    fix: Altera tamanho da logo na Lpi

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -97,7 +97,7 @@ export default function NavbarSocial({ LightMode }) {
         <Navbar>
           <ScrollLink
             to="home"
-            className="cursor-pointer w-[45%] phone2:w-[50%] phone3:w-[45%] tablet1:w-[30%] tablet2:w-[25%] desktop1:w-[25%] desktop2:max-w-[200px]"
+            className="cursor-pointer w-[45%] phone2:w-[50%] phone3:w-[45%] tablet1:w-[30%] tablet2:w-[25%] desktop1:w-[25%] desktop2:max-w-[190px]"
             spy={true}
             smooth={true}
             duration={500}
@@ -113,7 +113,7 @@ export default function NavbarSocial({ LightMode }) {
                     ? "bg-transparent w-[50%] tablet1:w-[50%] tablet2:w-[40%] desktop1:w-[60%] desktop2:w-[30%] transition-all duration-1000"
                     : "bg-white w-[64%] my-[20px] tablet1:w-[55%] tablet2:w-[47%] desktop1:w-[60%] desktop2:w-[40%] px-3 py-3 transition-all duration-1000"
                   : scrolling
-                  ? "bg-transparent w-[70%] phone3:w-[60%] tablet1:w-[80%] tablet2:w-[70%] desktop1:w-[80%] desktop2:w-[80%] transition-all duration-1000"
+                  ? "bg-transparent w-[70%] phone3:w-[60%] tablet1:w-[80%] tablet2:w-[70%] desktop1:w-[80%] desktop2:w-[70%] transition-all duration-1000"
                   : "bg-transparent my-[20px] w-full transition-all duration-1000"
               } tablet3:mb-0`}
             />
